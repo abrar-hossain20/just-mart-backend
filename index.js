94e825b1e98f080f85350322eadee486e90c3f8f
abrar-hossain20 <abrar1224noyon@gmail.com>
2026-01-23 16:55:48 +0600

    env file added

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://finalProject:kmfAkFMsjUhcxS8g@cluster0.4ofgl6a.mongodb.net/?appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4ofgl6a.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,9 +21,11 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
+
+    const db = client.db("Final_Project");
+    const productsCollection = db.collection("products");
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
